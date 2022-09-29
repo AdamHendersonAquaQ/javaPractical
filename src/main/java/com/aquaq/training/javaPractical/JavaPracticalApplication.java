bcc1a0ed695ad7945a644075151e3d004b9bacf3
AdamHendersonAquaQ <adam.henderson@aquaq.co.uk>
2022-09-29 13:27:09 +0000

    Initial commit of files

---
+++ src/main/java/com/aquaq/training/javaPractical/JavaPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.aquaq.training.javaPractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPracticalApplication.class, args);
+	}
+
+}
