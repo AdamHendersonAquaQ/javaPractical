aa68e26f956da66a59fb9a4a8bdc757ff13d319d
AdamHendersonAquaQ <adam.henderson@aquaq.co.uk>
2022-09-29 14:14:03 +0000

    Added SQL test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,3 +26,44 @@ CREATE TABLE StudentCourse
     FOREIGN KEY (courseId) REFERENCES Course(id),
     PRIMARY KEY (studentId, courseId)
 );
+
+INSERT INTO Student (firstName, lastName, graduationYear)
+VALUES ('Scott', 'Summers', '2024-01-01');
+INSERT INTO Student (firstName, lastName, graduationYear)
+VALUES ('Jean', 'Grey', '2024-01-01');
+INSERT INTO Student (firstName, lastName, graduationYear)
+VALUES ('Hank', 'McCoy', '2023-01-01');
+INSERT INTO Student (firstName, lastName, graduationYear)
+VALUES ('Robert', 'Drake', '2025-01-01');
+INSERT INTO Student (firstName, lastName, graduationYear)
+VALUES ('Warren', 'Worthington', '2024-01-01');
+
+INSERT INTO Course (courseName, subjectArea, creditAmount, studentCapacity, semesterCode)
+VALUES ('English', 'English', 5, 3, 'AUTUMN2022');
+INSERT INTO Course (courseName, subjectArea, creditAmount, studentCapacity, semesterCode)
+VALUES ('English Literature', 'English', 5, 5, 'WINTER2022');
+INSERT INTO Course (courseName, subjectArea, creditAmount, studentCapacity, semesterCode)
+VALUES ('Maths', 'Maths', 7, 3, 'AUTUMN2022');
+INSERT INTO Course (courseName, subjectArea, creditAmount, studentCapacity, semesterCode)
+VALUES ('Physics', 'Science', 5, 5, 'SPRING2023');
+INSERT INTO Course (courseName, subjectArea, creditAmount, studentCapacity, semesterCode)
+VALUES ('Chemistry', 'Science', 5, 5, 'SPRING2023');
+
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (1,1);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (2,1);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (3,2);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (4,3);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (5,3);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (1,4);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (3,4);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (3,5);
+INSERT INTO StudentCourse (studentId, courseId)
+VALUES (4,5);
