407e6cc93fb284dca492f5f30c09c5268f4ace89
AdamHendersonAquaQ <adam.henderson@aquaq.co.uk>
2022-10-03 14:53:11 +0000

    Added unit tests for semester query

--- src/main/java/com/aquaq/training/javaPractical/jdbc/StudentJdbcDao.java
+++ src/main/java/com/aquaq/training/javaPractical/jdbc/StudentJdbcDao.java
@@ -18,7 +18,7 @@ public class StudentJdbcDao {
     public List<Student> findAll() {
         List<Student> students = jdbcTemplate.query("select * from Student",
                 new BeanPropertyRowMapper<>(Student.class));
-        if(students.size()==0)
+        if (students.size() == 0)
             throw new StudentNotFoundException("No students found");
         else
             return students;
@@ -27,18 +27,29 @@ public class StudentJdbcDao {
     public Student findById(int id) {
         List<Student> students = jdbcTemplate.query("select * from Student where studentId=?",
                 new BeanPropertyRowMapper<>(Student.class), id);
-        if(students.size() == 0)
+        if (students.size() == 0)
             throw new StudentNotFoundException("Student id not found - " + id);
         else
             return students.get(0);
     }
 
     public Student findByStudentName(String firstName, String lastName) {
-        List<Student> students =  jdbcTemplate.query("select * from Student where firstName = ? AND lastName = ?",
+        List<Student> students = jdbcTemplate.query("select * from Student where firstName = ? AND lastName = ?",
                 new BeanPropertyRowMapper<>(Student.class), firstName, lastName);
-        if(students.size() == 0)
-            throw new StudentNotFoundException("Student name not found - " + firstName+" "+lastName);
+        if (students.size() == 0)
+            throw new StudentNotFoundException("Student name not found - " + firstName + " " + lastName);
         else
             return students.get(0);
+    }
+
+    public List<Student> findBySemester(String semesterCode) {
+        if (semesterCode.matches("^[A-Z]+[0-9]{4}$")) {
+            List<Student> students = jdbcTemplate.query("", new BeanPropertyRowMapper(Student.class), semesterCode);
+            if(students.size() == 0)
+                throw new StudentNotFoundException("Not students found for semester - " + semesterCode);
+            else
+                return students;
+        } else
+            throw new StudentNotFoundException("Semester not found - " + semesterCode);
     }
 }
